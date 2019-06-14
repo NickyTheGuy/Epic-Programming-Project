d7125b165416ea17f3e7ae964b8f7e445d907883
Kyle Halevi <kylehalevi@icloud.com>
2019-06-14 08:38:41 -0400

    Final

--- Epic Programming Project/ViewController.swift
+++ Epic Programming Project/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func Discover(_ sender: Any) {
+        
+        let NextImage = UIImageView(image: UIImage(named: "Wallpaper"))
+        NextImage.frame = view.bounds
+        NextImage.layer.position.x += view.frame.width
+        view.addSubview(NextImage)
+        
+        UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseInOut, animations: {
+            NextImage.layer.position.x -= self.view.frame.width
+            self.MainImage.layer.position.x -= self.view.frame.width
+        }, completion: {(finished: Bool) in
+            NextImage.removeFromSuperview()
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "PagesViewController") as! PagesViewController
+            vc.modalTransitionStyle = .crossDissolve
+            self.present(vc, animated: true, completion: nil)
+        })
+        
+    }
 }
 
