cac824ed1107d895f4ad5d87654c87f50f7ef13d
Kyle Halevi <kylehalevi@icloud.com>
2019-06-14 08:48:24 -0400

    Calendar

--- Epic Programming Project/FinalViewController.swift
+++ Epic Programming Project/FinalViewController.swift
@@ -7,30 +7,57 @@
 //
 
 import UIKit
+import EventKit
 
 class FinalViewController: UIViewController {
-
+    
     @IBOutlet weak var CalendarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         CalendarButton.setImage(UIImage(named: "Presseds"), for: .selected)
+        
+    }
+    
+    func addEvent(store: EKEventStore) {
+        let start = Date()
+        let end = start.addingTimeInterval(60)
+        
+        let newEvent = EKEvent(eventStore: store)
+        newEvent.calendar = store.defaultCalendarForNewEvents
+        newEvent.title = "Vote for Kenney 2020"
+        newEvent.startDate = start
+        newEvent.endDate = end
+        
+        do {
+            try store.save(newEvent, span: .thisEvent)
+        }catch let error as NSError {
+            print("Calendar failure: \(error)")
+        }
     }
     
     @IBAction func AddCalendar(_ sender: Any) {
         
+        let eventStore = EKEventStore()
         
+        eventStore.requestAccess(to: .event, completion: {
+            (granted: Bool, error: Error?) -> Void in
+            if granted {
+                self.addEvent(store: eventStore)
+                
+                DispatchQueue.main.asyncAfter(deadline: (DispatchTime.now() + 1)){
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+                    vc.modalTransitionStyle = .crossDissolve
+                    self.present(vc, animated: true, completion: nil)
+                }
+                
+                
+            }else{
+                exit(0)
+            }
+        })
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
